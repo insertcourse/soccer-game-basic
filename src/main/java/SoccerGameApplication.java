c99be373a02d964fa348238936de09d6486cd2e5
Yujin <jin37091289@gmail.com>
2023-04-14 02:10:14 +0900

    ADD : 공격수 2명(attacker1, attacker2)을 ArrayList로 선언

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class SoccerGameApplication {
     public static void main(String[] args) {
+        String[] attacker1 = {};
+        String[] attacker2 = {};
 
+        ArrayList<String> a1_list = Arrays.stream(attacker1)
+        .collect(Collectors.toCollection(ArrayList::new));
+
+        ArrayList<String> a2_list = Arrays.stream(attacker2)
+        .collect(Collectors.toCollection(ArrayList::new));
     }
 }
