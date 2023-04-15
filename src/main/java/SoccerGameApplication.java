b8cf77d642133a128f413fdb738d076cf79eeec8
jyj1289 <jin37091289@gmail.com>
2023-04-16 01:30:46 +0900

    refactor : 클래스 이름을 class 옆에 작성, attacker(1, 2),goalkeeper arrayList로 선언, 배열을 arrayList로 바꾸는 for문을 축약, 비교코드를 조금 더 축약, 결과 표시를 attacker1, attacker2로 수 정

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -1,23 +1,16 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class
-SoccerGameApplication {
+public class SoccerGameApplication {
     public static void main(String[] args) {
-        String[] attacker1 = {};
-        String[] attacker2 = {};
+        ArrayList<String> attacker1 = new ArrayList<String>();
 
-        ArrayList<String> a1_list = Arrays.stream(attacker1)
-             .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<String> attacker2 = new ArrayList<String>();
 
-        ArrayList<String> a2_list = Arrays.stream(attacker2)
-            .collect(Collectors.toCollection(ArrayList::new));
-
-        String[] goalkeeper = {};
-        ArrayList<String> g_list = Arrays.stream(goalkeeper)
-            .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<String> goalkeeper = new ArrayList<String>();
 
         Scanner sc = new Scanner(System.in);
         String[] valueString;
@@ -27,53 +20,45 @@ SoccerGameApplication {
         String value = sc.next();
         valueString = value.split(",");
 
-        for (int i = 0; i < 5; i++) {
-            a1_list.add(valueString[i]);
-        }
+        attacker1 = Arrays.stream(valueString)
+                        .collect(Collectors.toCollection(ArrayList::new));
 
         System.out.println("attacker2의 공을 입력해주십시오.");
         value = sc.next();
         valueString = value.split(",");
 
-        for (int i = 0; i < 5; i++){
-            a2_list.add(valueString[i]);
-        }
+        attacker2 = Arrays.stream(valueString)
+                        .collect(Collectors.toCollection(ArrayList::new));
 
         System.out.println("goalkeeper의 공을 입력해주십시오.");
         value = sc.next();
         valueString = value.split(",");
 
-
-        for (int i = 0; i < 4; i++){
-            g_list.add(valueString[i]);
-        }
+        goalkeeper = Arrays.stream(valueString)
+                .collect(Collectors.toCollection(ArrayList::new));
 
         int cnt1 = 0, cnt2 = 0;
-        int numberI, numberJ, numberK;
-        for (String i : g_list){
-            numberI = Integer.parseInt(i);
-            for (String j : a1_list){
-                numberJ = Integer.parseInt(j);
-                if (numberI == numberJ){
-                    cnt1 += 1;
+        for (String i : goalkeeper){
+            for (String j : attacker1){
+                if (Integer.parseInt(i) == Integer.parseInt(j)){
+                    cnt1++;
                 }
             }
-            for (String k : a2_list){
-                numberK = Integer.parseInt(k);
-                if (numberI == numberK){
-                    cnt2 += 1;
+            for (String k : attacker2){
+                if (Integer.parseInt(i) == Integer.parseInt(k)){
+                    cnt2++;
                 }
             }
         }
 
         if (cnt1 < cnt2){
-            System.out.println("attack1이 승리하였습니다.");
+            System.out.println("attacker1이 승리하였습니다.");
         }
         else if(cnt1 == cnt2){
             System.out.println("무승부입니다.");
         }
         else{
-            System.out.println("attack2이 승리하였습니다.");
+            System.out.println("attacker2이 승리하였습니다.");
         }
 
         sc.close();
