4bb69dcd0f14df91ff263833f8b89c2094f91849
Ubinquitous <ubinquitous1@gmail.com>
2023-04-17 10:36:03 +0900

    공격수 2명(attacker1, attacker2)을 ArrayList<String>로 선언을 한다.

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class SoccerGameApplication {
     public static void main(String[] args) {
-
+        ArrayList<String> attacker1 = new ArrayList<String>();
+        ArrayList<String> attacker2 = new ArrayList<String>();
     }
 }
