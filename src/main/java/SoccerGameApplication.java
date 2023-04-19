691130acb4a9650ec2a8482cd3cf22af01400a59
J1W0N-1209 <nabook754@naver.com>
2023-04-19 23:16:39 +0900

    공격수 2명 ArrayList로 선언

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class SoccerGameApplication {
     public static void main(String[] args) {
-
+        ArrayList<String> attacker1 = new ArrayList<>();
+        ArrayList<String> attacker2 = new ArrayList<>();
     }
 }
