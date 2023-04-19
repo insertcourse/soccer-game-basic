76ed3f0f0b21195c0379dd31df31b07f4bb6d558
J1W0N-1209 <nabook754@naver.com>
2023-04-20 00:26:01 +0900

    공격수,골키퍼 공을 입력 받는다

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -9,6 +9,18 @@ public class SoccerGameApplication {
         ArrayList<String> attacker1;
         ArrayList<String> attacker2;
         ArrayList<String> goalkeeper;
+        Scanner sc = new Scanner(System.in);
+
+        attacker1 = getPlayer("attacker1",sc);
+        attacker2 = getPlayer("attacker2",sc);
+        goalkeeper = getPlayer("goalkeeper",sc);
+    }
+
+    public static ArrayList<String> getPlayer(String player,Scanner sc) {
+        String arr[];
+        System.out.println(player+"의 공을 입력해주십시오.");
+        arr = sc.next().split(",");
+        return Arrays.stream(arr).collect(Collectors.toCollection(ArrayList::new));
     }
 }
 
